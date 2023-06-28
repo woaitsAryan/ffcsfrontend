598116f1ed9bf437852fe9fc0495579beacddb68
manaslaud <manaslaud@hotmail.com>
2023-06-29 00:28:56 +0530

    added css, login page #1 and assets

--- src/pages/login/auth.tsx
+++ src/pages/login/auth.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../../auth.css';
+
 
 const App = () => {
     const navigate = useNavigate();
   return (
-    <div>
-      <h1>This is the auth page!</h1>
-      <button onClick={() => navigate('/login')}>Go to Login</button>
-    <button onClick={() => navigate('/register')}>Go to Register</button>
+    <div className='container'>
+      <div>
+    <p className='step'>Step 1/<p className='step-number'>3</p></p>
+    <h2 className='header'>Hey,
+    <img src="Waving Hand.png" className='waving-emoji'></img>
+   </h2>
+   <h2 className='newline'>Let's get started</h2>
+    <p className='middle-txt'>Let's set up your account up and get to know you</p>
+    <div className='TnC-container'>
+    <input type="checkbox" id="myCheckbox" className="custom-checkbox" />
+    I agree to the Terms & Conditions and Privacy Policy
+    </div>
+    <div className='btn-container'>
+      <button className='btn-login' onClick={() => navigate('/login')}>Go to Login</button>
+    <button className='btn-signUp' onClick={() => navigate('/register')}>Go to Register</button>
+    </div>
+    </div>
     </div>
   );
 };
