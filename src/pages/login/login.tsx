0bdf384aa09beeec3436b889ebb48d3e4cdb2b4c
manaslaud <manaslaud@hotmail.com>
2023-06-29 13:16:33 +0530

    login flow done, FINALLY FIXED THE ANIMATION FFS

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,28 +1,28 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef,useEffect } from 'react';
 import { gsap } from 'gsap';
 import styles from '../../login.module.css'
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [step, setStep] = useState(1);
-
-  const stepRef = useRef(null);
+  let animationEl= useRef<HTMLDivElement>(null);
+  const stepRef =  useRef<HTMLDivElement>(null);
 
   const handleContinue = () => {
     if (step === 1) {
-      gsap.fromTo(stepRef.current, { x: window.innerWidth }, { x: 0, duration: 0.5, onStart: () => setStep(2) });
+      console.log(stepRef.current?.offsetHeight)
+      console.log(stepRef.current?.children)
+      gsap.fromTo(stepRef.current, { y: window.innerHeight}, { y: animationEl.current?.offsetTop,duration: 1.5,ease: "expo.out",onStart: () => setStep(2) });
+      // console.log(stepRef.current)
     } else if (step === 2) {
-      console.log(username, password);
-      // if login successful, save state and redirect to landing page
-      // else, display error message
+      // console.log(username, password);     
     }
   };
-
   return (
     <div>
-      <div ref={stepRef}>
+      <div ref={stepRef} className={styles.parent}>
         {step === 1 && (
-          <div className={styles.container}>
+          <div className={styles.container} ref={animationEl}  >
             <p className={styles.step}>Step 2/<p className={styles['step-number']}>3</p></p>
             <h2 className={styles['username-txt']}>Enter Username</h2>
             <input
@@ -35,14 +35,15 @@ const LoginForm = () => {
         )}
 
         {step === 2 && (
-          <div>
-            <h2>Enter Password</h2>
+          <div className={styles.container} >
+            <p className={styles.step}>Step 3/<p className={styles['step-number']}>3</p></p>
+            <h2 className={styles['username-txt']}>Enter Password</h2>
             <input
-              type="password"
-              value={password}
+              type="password" className={styles['username-field']}
+              value={password} placeholder='Enter password'
               onChange={(e) => setPassword(e.target.value)}
             />
-             <button onClick={handleContinue}>Continue</button>
+             <button onClick={handleContinue} className={styles['continue-btn']}>Continue</button>
           </div>
         )}
       </div>
