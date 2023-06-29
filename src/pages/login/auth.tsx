2c1037856e886f37d36d5d81a077dadbe7ae91c2
manaslaud <manaslaud@hotmail.com>
2023-06-29 11:35:13 +0530

    added css modules

--- src/pages/login/auth.tsx
+++ src/pages/login/auth.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../../auth.css';
+import styles from '../../auth.module.css';
 
 
 const App = () => {
     const navigate = useNavigate();
   return (
-    <div className='container'>
+    <div className={styles.container}>
       <div>
-    <p className='step'>Step 1/<p className='step-number'>3</p></p>
-    <h2 className='header'>Hey,
-    <img src="Waving Hand.png" className='waving-emoji'></img>
+    <p className={styles.step}>Step 1/<p className={styles['step-number']}>3</p></p>
+    <h2 className={styles.header}>Hey,
+    <img src="Waving Hand.png" className={styles['waving-emoji']}></img>
    </h2>
-   <h2 className='newline'>Let's get started</h2>
-    <p className='middle-txt'>Let's set up your account up and get to know you</p>
-    <div className='TnC-container'>
-    <input type="checkbox" id="myCheckbox" className="custom-checkbox" />
+   <h2 className={styles.newline}>Let's get started</h2>
+    <p className={styles['middle-txt']}>Let's set up your account up and get to know you</p>
+    <div className={styles['TnC-container']}>
+      <input type='checkbox'></input>
     I agree to the Terms & Conditions and Privacy Policy
     </div>
-    <div className='btn-container'>
-      <button className='btn-login' onClick={() => navigate('/login')}>Go to Login</button>
-    <button className='btn-signUp' onClick={() => navigate('/register')}>Go to Register</button>
+    <div className={styles['btn-container']}>
+      <button className={styles['btn-login']} onClick={() => navigate('/login')}>Go to Login</button>
+    <button className={styles['btn-signUp']} onClick={() => navigate('/register')}>Go to Register</button>
     </div>
     </div>
     </div>
