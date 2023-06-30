a2c4584b56029d2bb60f04041451f1077a53a00c
manaslaud <manaslaud@hotmail.com>
2023-06-30 12:48:41 +0530

    implemented fade transitions

--- src/pages/login/auth.tsx
+++ src/pages/login/auth.tsx
@@ -1,39 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../css/auth.module.css';
 
+const App: React.FC = () => {
+  const navigate = useNavigate();
+  const [fadeOut, setFadeOut] = useState(false);
 
-const App = () => {
-    const navigate = useNavigate();
-
-    const handleLogin = (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault(); 
+  const handleLogin = () => {
+    setFadeOut(true);
+    setTimeout(() => {
       navigate('/login');
-    };
-  
-    const handleRegister = (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault(); 
+    }, 500); // Adjust the duration of the fade-out animation here
+  };
+
+  const handleRegister = () => {
+    setFadeOut(true);
+    setTimeout(() => {
       navigate('/register');
-    };
+    }, 500); // Adjust the duration of the fade-out animation here
+  };
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${fadeOut ? styles['fade-out'] : ''}`}>
       <div>
-    <p className={styles.step}>Step 1/<p className={styles['step-number']}>3</p></p>
-    <h2 className={styles.header}>Hey,
-    <img src="Waving Hand.png" className={styles['waving-emoji']}></img>
-   </h2>
-   <h2 className={styles.newline}>Let's get started</h2>
-    <p className={styles['middle-txt']}>Let's set up your account up and get to know you</p>
-    <div className={styles['TnC-container']}>
-      <input type='checkbox'></input>
-    I agree to the Terms & Conditions and Privacy Policy
-    </div>
-    <div className={styles['btn-container']}>
-      <button className={styles['btn-login']} onClick={handleLogin}>Go to Login</button>
-    <button className={styles['btn-signUp']} onClick={handleRegister}>Go to Register</button>
-    </div>
-    </div>
+        <p className={styles.step}>Step 1/<p className={styles['step-number']}>3</p></p>
+        <h2 className={styles.header}>
+          Hey,
+          <img src="Waving Hand.png" className={styles['waving-emoji']} alt="Waving Hand"></img>
+        </h2>
+        <h2 className={styles.newline}>Let's get started</h2>
+        <p className={styles['middle-txt']}>Let's set up your account up and get to know you</p>
+        <div className={styles['TnC-container']}>
+          <input type='checkbox' />
+          I agree to the Terms & Conditions and Privacy Policy
+        </div>
+        <div className={styles['btn-container']}>
+          <button className={styles['btn-login']} onClick={handleLogin}>Go to Login</button>
+          <button className={styles['btn-signUp']} onClick={handleRegister}>Go to Register</button>
+        </div>
+      </div>
     </div>
   );
 };
