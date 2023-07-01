a90f5b4b6dc7be727893ee2a566c32fead1d3586
manaslaud <manaslaud@hotmail.com>
2023-07-01 23:34:44 +0530

    added assets

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import Hero from '../components/Hero';
 import { ButtonProvider } from '../buttonContext';
 import Styles from '../css/landing.module.css'
+import CourseType from '../components/CourseType';
+
 
 const Landing = () => {
  
@@ -14,6 +16,10 @@ const Landing = () => {
       <Hero />
     </ButtonProvider>
     <p className={Styles['course-type']}>Course Type</p>
+    <div className={Styles['courseTypeContainer']}>
+    <CourseType name='Discipline Core' imagePath='Flask Icon.svg'></CourseType>
+    <CourseType name=' Core' imagePath='Gear Icon.svg'></CourseType>
+    </div>
     </div>
   );
 };
