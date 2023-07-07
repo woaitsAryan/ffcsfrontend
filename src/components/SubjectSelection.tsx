7d2e3426479e6cddfa591a8610104f0ac02ffbbd
Armaan <indaniarmaan4@gmail.com>
2023-07-07 17:01:43 +0530

    Subject Selection table added

---
+++ src/components/SubjectSelection.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import styles from "../css/subjectSelection.module.css";
+
+const SubjectSelection: React.FC = () => {
+  return (
+    <div className={styles.SubjectSelectionContainer}>
+      <table className={styles.SubjectSelection}>
+        <thead className={styles.head}>
+          <tr>
+            <th className={styles.tableHeadings}>Pick</th>
+            <th className={styles.tableHeadings}>S. No.</th>
+            <th className={styles.tableHeadings}>Course Code</th>
+            <th className={styles.tableHeadings}>Course Title</th>
+            <th className={styles.tableHeadings}>Version</th>
+            <th className={styles.tableHeadings}>L</th>
+            <th className={styles.tableHeadings}>T</th>
+            <th className={styles.tableHeadings}>P</th>
+            <th className={styles.tableHeadings}>J</th>
+            <th className={styles.tableHeadings}>Credits</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.tableData}>[ ]</td>
+            <td className={styles.tableData}>1</td>
+            <td className={styles.tableData}>BEEE102L</td>
+            <td className={styles.tableData}>
+              Basic Electrical and Electronics...
+            </td>
+            <td className={styles.tableData}>1.0</td>
+            <td className={styles.tableData}>3.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>3.0</td>
+          </tr>
+          <tr>
+            <td className={styles.tableData}>[ ]</td>
+            <td className={styles.tableData}>2</td>
+            <td className={styles.tableData}>BCSE101E</td>
+            <td className={styles.tableData}>Computer Programming: Python</td>
+            <td className={styles.tableData}>1.0</td>
+            <td className={styles.tableData}>1.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>4.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>3.0</td>
+          </tr>
+          <tr>
+            <td className={styles.tableData}>[ ]</td>
+            <td className={styles.tableData}>3</td>
+            <td className={styles.tableData}>BCSE103E</td>
+            <td className={styles.tableData}>Computer Programming: Java</td>
+            <td className={styles.tableData}>1.0</td>
+            <td className={styles.tableData}>1.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>4.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>3.0</td>
+          </tr>
+          <tr>
+            <td className={styles.tableData}>[ ]</td>
+            <td className={styles.tableData}>4</td>
+            <td className={styles.tableData}>BHUM103L</td>
+            <td className={styles.tableData}>Micro Economics</td>
+            <td className={styles.tableData}>1.0</td>
+            <td className={styles.tableData}>3.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>3.0</td>
+          </tr>
+          <tr>
+            <td className={styles.tableData}>[ ]</td>
+            <td className={styles.tableData}>5</td>
+            <td className={styles.tableData}>BEEE102L</td>
+            <td className={styles.tableData}>Urban Community Development</td>
+            <td className={styles.tableData}>1.0</td>
+            <td className={styles.tableData}>3.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>0.0</td>
+            <td className={styles.tableData}>3.0</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default SubjectSelection;
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,50 +1,71 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 // import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import { ButtonProvider } from '../buttonContext';
-import Styles from '../css/landing.module.css'
-import CourseType from '../components/CourseType';
-import Slots from '../components/Slots';
-import Timetable from '../components/Timetable';
-import Footer from '../components/Footer';
-
+import Hero from "../components/Hero";
+import { ButtonProvider } from "../buttonContext";
+import Styles from "../css/landing.module.css";
+import CourseType from "../components/CourseType";
+import Slots from "../components/Slots";
+import Timetable from "../components/Timetable";
+import Footer from "../components/Footer";
+import SubjectSelection from "../components/SubjectSelection";
 const Landing = () => {
- 
-
   return (
     <div>
       <div className={Styles.mainflexbox}>
-    <ButtonProvider>
-      <Hero />
-    </ButtonProvider>
-    </div>
-    <div className={Styles.mainflexbox2}>
-    <p className={Styles.title}>Course Type</p>
-    
-    <div className={`${Styles.courseTypeContainer} ${Styles.snapsInline}`}>
-    {/* <div className={Styles.gradeint}></div> */}
-    <CourseType name='Discipline Core' imagePath='Flask Icon.svg'></CourseType>
-    <CourseType name='Open Elective' imagePath='Gear Icon.svg'></CourseType>
-    <CourseType name='Foundation Core' imagePath='Window Icon.svg'></CourseType>
-    <CourseType name='Discipline Elective' imagePath='Hammer Icon.svg'></CourseType>
-    <CourseType name='Discipline Linked Engineering Sciences' imagePath='Tools Icon.svg'></CourseType>
-    <CourseType name='Non Graded Core Requirement' imagePath='Guitar Icon.svg'></CourseType>
-    <CourseType name='Projects and Internship' imagePath='Shopping Basket.svg'></CourseType>
-    </div>
-    </div>
-    <p className={Styles.title}>Course Slots</p>
-    <div className={Styles.slotsContainer}>
-    <Slots></Slots>
-    <Slots></Slots>
-    <Slots></Slots>
-    <Slots></Slots>
-    <Slots></Slots>
-    <Slots></Slots>  
-    </div>
-    <p className={Styles.title}>Timetable</p>
-    <Timetable></Timetable>
-    <Footer></Footer>
+        <ButtonProvider>
+          <Hero />
+        </ButtonProvider>
+      </div>
+      <div className={Styles.mainflexbox2}>
+        <p className={Styles.title}>Course Type</p>
+
+        <div className={`${Styles.courseTypeContainer} ${Styles.snapsInline}`}>
+          {/* <div className={Styles.gradeint}></div> */}
+          <CourseType
+            name="Discipline Core"
+            imagePath="Flask Icon.svg"
+          ></CourseType>
+          <CourseType
+            name="Open Elective"
+            imagePath="Gear Icon.svg"
+          ></CourseType>
+          <CourseType
+            name="Foundation Core"
+            imagePath="Window Icon.svg"
+          ></CourseType>
+          <CourseType
+            name="Discipline Elective"
+            imagePath="Hammer Icon.svg"
+          ></CourseType>
+          <CourseType
+            name="Discipline Linked Engineering Sciences"
+            imagePath="Tools Icon.svg"
+          ></CourseType>
+          <CourseType
+            name="Non Graded Core Requirement"
+            imagePath="Guitar Icon.svg"
+          ></CourseType>
+          <CourseType
+            name="Projects and Internship"
+            imagePath="Shopping Basket.svg"
+          ></CourseType>
+        </div>
+      </div>
+      <p className={Styles.title}>Course Slots</p>
+      <div className={Styles.slotsContainer}>
+        <Slots></Slots>
+        <Slots></Slots>
+        <Slots></Slots>
+        <Slots></Slots>
+        <Slots></Slots>
+        <Slots></Slots>
+      </div>
+      <p className={Styles.title}>Subject Selection</p>
+      <SubjectSelection></SubjectSelection>
+      <p className={Styles.title}>Timetable</p>
+      <Timetable></Timetable>
+      <Footer></Footer>
     </div>
   );
 };
