9f20ce75eb3b2f975b73d16f6c58e6ec08acbc5b
Armaan <indaniarmaan4@gmail.com>
2023-07-07 13:17:46 +0530

    Did CSS for mobile size of auth, login, register

--- src/pages/login/auth.tsx
+++ src/pages/login/auth.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import styles from '../../css/auth.module.css';
-let val='';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import styles from "../../css/auth.module.css";
+let val = "";
 interface LocationState {
   buttonClicked: string;
 }
@@ -10,9 +10,8 @@ const AuthPage: React.FC = () => {
   const location = useLocation();
   const buttonClicked = (location.state as LocationState)?.buttonClicked;
   console.log(buttonClicked);
-  val=buttonClicked;
-  useEffect(() => {
-  },[buttonClicked]);
+  val = buttonClicked;
+  useEffect(() => {}, [buttonClicked]);
 
   const navigate = useNavigate();
   const [fadeOut, setFadeOut] = useState(false);
@@ -20,39 +19,53 @@ const AuthPage: React.FC = () => {
   const handleLogin_ = () => {
     setFadeOut(true);
     setTimeout(() => {
-      navigate('/login');
+      navigate("/login");
     }, 500); // Adjust the duration of the fade-out animation here
   };
 
   const handleRegister_ = () => {
     setFadeOut(true);
     setTimeout(() => {
-      navigate('/register');
+      navigate("/register");
     }, 500); // Adjust the duration of the fade-out animation here
   };
 
   return (
-    <div className={`${styles.container} ${fadeOut ? styles['fade-out'] : ''}`}>
+    <div className={`${styles.container} ${fadeOut ? styles["fade-out"] : ""}`}>
       <div>
         <p className={styles.step}>
           Step 1/
-          <p className={styles['step-number']}>3</p>
+          <p className={styles["step-number"]}>3</p>
         </p>
         <h2 className={styles.header}>
           Hey,
-          <img src="Waving Hand.png" className={styles['waving-emoji']} alt="Waving Hand" />
+          <img
+            src="Waving Hand.png"
+            className={styles["waving-emoji"]}
+            alt="Waving Hand"
+          />
         </h2>
         <h2 className={styles.newline}>Let's get started</h2>
-        <p className={styles['middle-txt']}>Let's set up your account up and get to know you</p>
-        <div className={styles['TnC-container']}>
-          <input type="checkbox" />
-          I agree to the Terms & Conditions and Privacy Policy
+        <p className={styles["middle-txt"]}>
+          Let's set up your account up and get to know you
+        </p>
+        <div className={styles["TnC-container"]}>
+          <input type="checkbox" />I agree to the Terms & Conditions and Privacy
+          Policy
         </div>
-        <div className={styles['btn-container']}>
-          <button className={val==='login'?styles['btn-login']:styles['invis']} onClick={handleLogin_}>
+        <div className={styles["btn-container"]}>
+          <button
+            className={val === "login" ? styles["btn-login"] : styles["invis"]}
+            onClick={handleLogin_}
+          >
             Go to Login
           </button>
-          <button className={val==='register'?styles['btn-signUp']:styles['invis']} onClick={handleRegister_}>
+          <button
+            className={
+              val === "register" ? styles["btn-signUp"] : styles["invis"]
+            }
+            onClick={handleRegister_}
+          >
             Go to Register
           </button>
         </div>
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,78 +1,102 @@
-import React, { useState, useRef,useEffect } from 'react';
-import { gsap } from 'gsap';
-import { useNavigate } from 'react-router-dom';
-import styles from '../../css/login.module.css'
-import postHandler from '../../handlers/postHandler';
-import Cookies from 'js-cookie';
+import React, { useState, useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { useNavigate } from "react-router-dom";
+import styles from "../../css/login.module.css";
+import postHandler from "../../handlers/postHandler";
+import Cookies from "js-cookie";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [step, setStep] = useState(1);
-  let animationEl= useRef<HTMLDivElement>(null);
-  const stepRef =  useRef<HTMLDivElement>(null);
-  const animationheight = { from: window.innerHeight, to: window.innerHeight/4 };
+  let animationEl = useRef<HTMLDivElement>(null);
+  const stepRef = useRef<HTMLDivElement>(null);
+  const animationheight = {
+    from: window.innerHeight,
+    to: window.innerHeight / 4,
+  };
   const navigate = useNavigate();
 
   useEffect(() => {
     if (step === 1 && animationEl.current) {
-      console.log(window.innerHeight, animationEl.current.offsetTop)
+      console.log(window.innerHeight, animationEl.current.offsetTop);
       gsap.fromTo(
         stepRef.current,
         { y: animationheight.from },
         {
           y: animationheight.to,
           duration: 1.5,
-          ease: 'expo.out',
+          ease: "expo.out",
         }
       );
     }
   }, []);
-  const handleContinue = ()  => {
+  const handleContinue = () => {
     if (step === 1) {
-      gsap.fromTo(stepRef.current, { y: animationheight.from}, { y: animationheight.to,duration: 1.5,ease: "expo.out",onStart: () => setStep(2) });
+      gsap.fromTo(
+        stepRef.current,
+        { y: animationheight.from },
+        {
+          y: animationheight.to,
+          duration: 1.5,
+          ease: "expo.out",
+          onStart: () => setStep(2),
+        }
+      );
       // console.log(stepRef.current)
     } else if (step === 2) {
-      const payload = {username: username, password: password};
-      postHandler('http://127.0.0.1:3000/login',payload, false)
-      .then((response) => {
-        const {token} = response.data;
-        Cookies.set('token',token, { expires: 30 });
-        navigate('/');
-      })
+      const payload = { username: username, password: password };
+      postHandler("http://127.0.0.1:3000/login", payload, false, "token").then(
+        (response) => {
+          const { token } = response.data;
+          localStorage.setItem("token", token);
+          Cookies.set("token", token, { expires: 30 });
+          navigate("/");
+        }
+      );
     }
   };
   return (
     <div className={styles.mainContainer}>
       <div ref={stepRef}>
         {step === 1 && (
-          <div className={styles.container} ref={animationEl}  >
-            <p className={styles.step}>Step {step+1}/<p className={styles['step-number']}>3</p></p>
-            <h2 className={styles['username-txt']}>Enter Username</h2>
+          <div className={styles.container} ref={animationEl}>
+            <p className={styles.step}>
+              Step {step + 1}/<p className={styles["step-number"]}>3</p>
+            </p>
+            <h2 className={styles["username-txt"]}>Enter Username</h2>
             <input
-              type="text" placeholder='Enter Username' className={styles['username-field']}
+              type="text"
+              placeholder="Enter Username"
+              className={styles["username-field"]}
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
-             <button className={styles['continue-btn']} onClick={handleContinue}>Continue</button>
+            <button className={styles["continue-btn"]} onClick={handleContinue}>
+              Continue
+            </button>
           </div>
         )}
 
         {step === 2 && (
-          <div className={styles.container} >
-            <p className={styles.step}>Step {step+1}/<p className={styles['step-number']}>3</p></p>
-            <h2 className={styles['username-txt']}>Enter Password</h2>
+          <div className={styles.container}>
+            <p className={styles.step}>
+              Step {step + 1}/<p className={styles["step-number"]}>3</p>
+            </p>
+            <h2 className={styles["username-txt"]}>Enter Password</h2>
             <input
-              type="password" className={styles['username-field']}
-              value={password} placeholder='Enter password'
+              type="password"
+              className={styles["username-field"]}
+              value={password}
+              placeholder="Enter password"
               onChange={(e) => setPassword(e.target.value)}
             />
-             <button onClick={handleContinue} className={styles['continue-btn']}>Continue</button>
+            <button onClick={handleContinue} className={styles["continue-btn"]}>
+              Continue
+            </button>
           </div>
         )}
       </div>
-
-     
     </div>
   );
 };
