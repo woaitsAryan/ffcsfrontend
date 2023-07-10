0bd598c6f0f8c945efe8293cab5c387f8c3ea241
Ritika Agrawal <ritikaagrawal339@gmail.com>
2023-07-11 04:56:15 +0530

    fix: Removed Extra Folder

--- src/components/CourseType.tsx
+++ src/components/CourseType.tsx
@@ -1,40 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../css/coursetype.module.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import styles from "../css/coursetype.module.css";
+import { useNavigate } from "react-router-dom";
 
 const MAX_CHAR = 20;
 
 interface CourseTypeProps {
-    name: string;
-    imagePath: string;
-    onClick: () => void;
-  }
+  name: string;
+  imagePath: string;
+  onClick: () => void;
+}
 
-  const CourseType: React.FC<CourseTypeProps> = ({ name, imagePath, onClick }) => {
+const CourseType: React.FC<CourseTypeProps> = ({
+  name,
+  imagePath,
+  onClick,
+}) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const limitCharacters = (text:string, maxLength:number) => {
-      if (text.length <= maxLength) {
-        return text;
-      }
-      return text.substring(0, maxLength) + '...'; 
-    };
-  
-    const limitedName = limitCharacters(name, MAX_CHAR);
-
-    const handleClick = () => {
-      onClick();
-      navigate('/')
+  const limitCharacters = (text: string, maxLength: number) => {
+    if (text.length <= maxLength) {
+      return text;
     }
+    return text.substring(0, maxLength) + "...";
+  };
 
-    return (
-      
-      <button className={styles.container} onClick={handleClick}>
+  const limitedName = limitCharacters(name, MAX_CHAR);
+
+  const handleClick = () => {
+    onClick();
+    navigate("/");
+  };
+
+  return (
+    <button className={styles.container} onClick={handleClick}>
       <img className={styles.image} src={imagePath} alt="Course" />
       <p className={styles.title}>{limitedName}</p>
-      </button>
-    );
-  };
+    </button>
+  );
+};
 
 export default CourseType;
