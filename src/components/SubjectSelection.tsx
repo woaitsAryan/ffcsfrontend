e37e91fe3b8999de0f99eab32b7cbe37509a021c
manaslaud <manaslaud@hotmail.com>
2023-07-11 22:44:15 +0530

    added pagination

--- src/components/SubjectSelection.tsx
+++ src/components/SubjectSelection.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "../css/subjectSelection.module.css";
 import getHandler from "../handlers/getHandler";
-import {BarLoader} from "react-spinners"
-
+import { BarLoader } from "react-spinners";
 
 interface Subject {
   code: string;
@@ -30,10 +29,18 @@ interface SubjectSelectionProps {
   timetablenum: number;
 }
 
-const SubjectSelection: React.FC<SubjectSelectionProps> = ({selectedCourseType, onCheckboxChange, onSubjectChange, timetablenum}) => {
-  const [loader,showBarLoader] = useState(false);
+const ITEMS_PER_PAGE = 5; // Number of items to display per page
+
+const SubjectSelection: React.FC<SubjectSelectionProps> = ({
+  selectedCourseType,
+  onCheckboxChange,
+  onSubjectChange,
+  timetablenum,
+}) => {
+  const [loader, showBarLoader] = useState(false);
   const [subjects, setSubjects] = useState<Subject[]>([]);
   const [selectedSubject, setSelectedSubject] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchSubjects = async () => {
@@ -55,18 +62,32 @@ const SubjectSelection: React.FC<SubjectSelectionProps> = ({selectedCourseType, 
     onSubjectChange("");
   }, [timetablenum]);
 
-  const handleSubjectNameChange = (name:string) => {
+  const handleSubjectNameChange = (name: string) => {
     onSubjectChange(name);
-  }
+  };
+
   const handleCheckboxClick = (subject: Subject) => {
     onCheckboxChange(subject.slots);
-    handleSubjectNameChange(subject.code); 
+    handleSubjectNameChange(subject.code);
     setSelectedSubject(subject.code);
   };
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const getPaginatedItems = (items: any[], currentPage: number, itemsPerPage: number) => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return items.slice(startIndex, endIndex);
+  };
+
+  const paginatedSubjects = getPaginatedItems(subjects, currentPage, ITEMS_PER_PAGE);
 
   return (
     <div className={styles.SubjectSelectionContainer}>
       <table className={styles.SubjectSelection}>
+        {/* Table Head */}
         <thead className={styles.head}>
           <tr>
             <th className={styles.tableHeadings}>Pick</th>
@@ -81,31 +102,60 @@ const SubjectSelection: React.FC<SubjectSelectionProps> = ({selectedCourseType, 
             <th className={styles.tableHeadings}>Credits</th>
           </tr>
         </thead>
+        {/* Table Body */}
         <tbody>
-          {
-          subjects !== undefined && subjects.length > 0 && loader===true ? 
-            (subjects.map((subject,index) => (
-            <tr key={subject.code}>
-            <td className={styles.tableData}>
-              <input type="radio" onClick={() => handleCheckboxClick(subject)} checked = {subject.code === selectedSubject}/>
-            </td>
-            <td className={styles.tableData}>{index + 1}</td>
-            <td className={styles.tableData}>{subject.code}</td>
-            <td className={styles.tableData}>{subject.name}</td>
-            <td className={styles.tableData}>1.0</td>
-            <td className={styles.tableData}>3.0</td>
-            <td className={styles.tableData}>0.0</td>
-            <td className={styles.tableData}>0.0</td>
-            <td className={styles.tableData}>0.0</td>
-            <td className={styles.tableData}>{subject.credits}</td>
-          </tr>
-          ))):( subjects===undefined ?<tr>
-            <td colSpan={10} className={styles.tableData}>No subjects found</td>
-         </tr>:
-         <BarLoader color="#36d7b7" width='100%'/>
-                     )}  
+          {loader ? (
+            paginatedSubjects.map((subject, index) => (
+              <tr key={subject.code}>
+                <td className={styles.tableData}>
+                  <input
+                    type="radio"
+                    onClick={() => handleCheckboxClick(subject)}
+                    checked={subject.code === selectedSubject}
+                  />
+                </td>
+                <td className={styles.tableData}>{index + 1}</td>
+                <td className={styles.tableData}>{subject.code}</td>
+                <td className={styles.tableData}>{subject.name}</td>
+                <td className={styles.tableData}>1.0</td>
+                <td className={styles.tableData}>3.0</td>
+                <td className={styles.tableData}>0.0</td>
+                <td className={styles.tableData}>0.0</td>
+                <td className={styles.tableData}>0.0</td>
+                <td className={styles.tableData}>{subject.credits}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={10} className={styles.tableData}>
+                <BarLoader color="#36d7b7" width="100%" />
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
+      {/* Pagination */}
+      {loader && subjects.length > ITEMS_PER_PAGE && (
+        <div className={styles.pagination}>
+          <button
+            className={`${styles.paginationButton} ${currentPage === 1 ? styles.disabled : ""}`}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <span className={styles.pageNumber}>{currentPage}</span>
+          <button
+            className={`${styles.paginationButton} ${
+              currentPage === Math.ceil(subjects.length / ITEMS_PER_PAGE) ? styles.disabled : ""
+            }`}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === Math.ceil(subjects.length / ITEMS_PER_PAGE)}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 };
